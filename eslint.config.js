99c00105afb53f3dd3e005a8af79da27021b6cf1
h <h@192.168.88.189>
2026-04-27 20:08:47 +0800

    Re-enable all checks

---
+++ eslint.config.js
@@ -0,0 +1,13 @@
+import reactNative from '@react-native/eslint-config';
+import prettier from 'eslint-config-prettier';
+
+export default [
+  ...reactNative,
+  prettier,
+  {
+    rules: {
+      'prettier/prettier': 'error',
+      'no-console': 'off',
+    },
+  },
+];
